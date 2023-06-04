b7e2bd82f077a8d91ee6dc8602454d520171e4c1
Dominik <dominikgos3@gmail.com>
2023-06-04 19:37:02 +0200

    added question store tests

--- tests/Feature/QuestionTest.php
+++ tests/Feature/QuestionTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class QuestionTest extends TestCase
@@ -40,5 +43,58 @@ class QuestionTest extends TestCase
         $response
             ->assertOk()
             ->assertJsonIsArray('questions');
+    }
+
+    public function test_user_can_store_question_with_correct_credentials(): void
+    {
+        $user = $this->user;
+
+        Sanctum::actingAs($user);
+
+        $quiz = $this->quiz;
+        $question = Question::factory()->for($quiz)->make();
+
+        $response = $this->postJson(route('questions.store'), [
+            'quiz_id' => $quiz->id,
+            'content' => $question->content
+        ]);
+
+        $response
+            ->assertCreated()
+            ->assertJsonPath('question.content', $question->content);
+    }
+
+    public function test_user_cannot_store_question_with_incorrect_credentials(): void
+    {
+        $user = $this->user;
+
+        Sanctum::actingAs($user);
+
+        $response = $this->postJson(route('questions.store'), [
+            'quiz_id' => null,
+            'content' => null
+        ]);
+
+        $response
+            ->assertUnprocessable();
+    }
+
+    public function test_user_cannot_store_question_in_quiz_that_does_not_belong_to_him(): void
+    {
+        $user = User::factory()->create();
+        $authorOfQuiz = $this->user;
+
+        Sanctum::actingAs($user);
+
+        $quiz = $this->quiz;
+        $question = Question::factory()->for($quiz)->make();
+
+        $response = $this->postJson(route('questions.store'), [
+            'quiz_id' => $quiz->id,
+            'content' => $question->content
+        ]);
+
+        $response
+            ->assertForbidden();
     }
 }
