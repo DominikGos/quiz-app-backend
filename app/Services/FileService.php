a28f9190c6829459682aa8e270f67ed6a82ce3a7
Dominik <dominikgos3@gmail.com>
2023-06-06 23:11:03 +0200

    added file service

---
+++ app/Services/FileService.php
@@ -0,0 +1,31 @@
+
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class FileService
+{
+    private string $directory;
+    private string $disk;
+
+    public function __construct(string $directory, string $disk)
+    {
+        $this->directory = $directory;
+        $this->disk = $disk;
+    }
+
+    public function store(UploadedFile $file): string
+    {
+        $path = $file->store($this->directory, $this->disk);
+
+        return asset('storage/' . $path);
+    }
+
+    public function destroy(string $filePath): void
+    {
+        Storage::disk($this->disk)->delete($filePath);
+    }
+}
